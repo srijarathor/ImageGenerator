cda8ae1360ac6320a3237ba6ff56bcb4c0b917eb
srijarathor <64653668+srijarathor@users.noreply.github.com>
2023-03-26 13:04:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const path=require("path");
+const express = require("express");
+const dotenv=require("dotenv").config();
+
+const app=express();
+
+const port=process.env.PORT||5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port,()=>{
+    console.log(`Server started on Port ${port}`);
+})
